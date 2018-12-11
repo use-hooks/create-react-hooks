b39416b2de0b380955dfd54328abad88257455d3
Cody Chan <int64ago@gmail.com>
2018-12-11 23:42:05 +0800

    feat: custom hooks name

--- bin/init.js
+++ bin/init.js
@@ -6,14 +6,21 @@ var fs = require('fs');
 var vfs = require('vinyl-fs');
 var through = require('through2');
 var path = require('path');
+var ejs = require('ejs');
 var join = path.join;
 var basename = path.basename;
 
-if (process.argv.length === 3 &&
-  (process.argv[2] === '-v' || process.argv[2] === '--version')) {
+if (process.argv.length < 3) {
+  console.log('Usage: npx create-react-hooks <name-of-hooks>');
+  return;
+}
+
+if (process.argv[2] === '-v' || process.argv[2] === '--version') {
   console.log(require('../package').version);
   return;
 }
+
+var hooksName = process.argv[2];
 
 init();
 
@@ -22,7 +29,7 @@ function init() {
   var dest = process.cwd();
 
   vfs.src(['**/*', '!node_modules/**/*'], { cwd: cwd, cwdbase: true, dot: true })
-    .pipe(template(dest))
+    .pipe(template(dest, cwd))
     .pipe(vfs.dest(dest))
     .on('end', function () {
       fs.renameSync(path.join(dest, 'gitignore'), path.join(dest, '.gitignore'));
@@ -31,18 +38,24 @@ function init() {
     .resume();
 }
 
-function template(dest) {
+function template(dest, cwd) {
   return through.obj(function (file, enc, cb) {
     if (!file.stat.isFile()) {
       return cb();
     }
 
-    console.log('Write %s', simplifyFilename(join(dest, basename(file.path))));
+    var resultStr = ejs.render(file.contents.toString('utf8'), {
+      name: hooksName
+    });
+
+    file.contents = new Buffer(resultStr, 'utf8');
+
+    console.log('Write %s', simplifyFilename(file.path, cwd));
     this.push(file);
     cb();
   });
 }
 
-function simplifyFilename(filename) {
-  return filename.replace(process.cwd(), ".");
+function simplifyFilename(filename, cwd) {
+  return filename.replace(cwd, ".");
 }
