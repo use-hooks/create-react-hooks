a97d9011b2e960e9ab30f8eebb15709601a2ca9a
Cody Chan <int64ago@gmail.com>
2018-12-13 15:15:58 +0800

    feat: use inquire

--- bin/init.js
+++ bin/init.js
@@ -2,34 +2,51 @@
 
 // Ref: https://github.com/ant-design/antd-init/blob/master/bin/antd-init
 
-var fs = require('fs');
-var vfs = require('vinyl-fs');
-var through = require('through2');
-var path = require('path');
-var ejs = require('ejs');
-var join = path.join;
-var basename = path.basename;
-
-if (process.argv.length < 3) {
-  console.log('Usage: npx create-react-hooks <name-of-hooks>');
-  return;
-}
+const fs = require('fs');
+const vfs = require('vinyl-fs');
+const through = require('through2');
+const path = require('path');
+const ejs = require('ejs');
+const inquirer = require('inquirer');
+const join = path.join;
+const basename = path.basename;
 
 if (process.argv[2] === '-v' || process.argv[2] === '--version') {
   console.log(require('../package').version);
   return;
 }
 
-var hooksName = process.argv[2];
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: "The name of Hooks:",
+    default: 'name-of-hooks',
+    validate: function(input) {
+      if (!input) return 'The name is required!';
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    name: 'desc',
+    message: "The description of Hooks:",
+    default: 'Custom React Hooks',
+    validate: function(input) {
+      if (!input) return 'The description is required!';
+      return true;
+    }
+  }
+]).then(answers => {
+  init(answers);
+});
 
-init();
-
-function init() {
-  var cwd = join(__dirname, '../scaffold');
-  var dest = process.cwd();
+function init(answers) {
+  const cwd = join(__dirname, '../scaffold');
+  const dest = process.cwd();
 
   vfs.src(['**/*', '!node_modules/**/*'], { cwd: cwd, cwdbase: true, dot: true })
-    .pipe(template(dest, cwd))
+    .pipe(template(dest, cwd, answers))
     .pipe(vfs.dest(dest))
     .on('end', function () {
       fs.renameSync(path.join(dest, '_gitignore'), path.join(dest, '.gitignore'));
@@ -39,15 +56,13 @@ function init() {
     .resume();
 }
 
-function template(dest, cwd) {
+function template(dest, cwd, answers) {
   return through.obj(function (file, enc, cb) {
     if (!file.stat.isFile()) {
       return cb();
     }
 
-    var resultStr = ejs.render(file.contents.toString('utf8'), {
-      name: hooksName
-    });
+    const resultStr = ejs.render(file.contents.toString('utf8'), answers);
 
     file.contents = new Buffer(resultStr, 'utf8');
 
