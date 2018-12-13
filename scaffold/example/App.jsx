3da148c251178101222405921e5fd5a757ed97c6
Cody Chan <int64ago@gmail.com>
2018-12-13 14:51:53 +0800

    feat: update eslint && scripts

--- scaffold/example/App.jsx
+++ scaffold/example/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
 
   return (
     <div>
-      <h2>DEMO of <span style={{ color: '#F44336' }}>@use-hooks/<%= name %></span></h2>
+      <h2>DEMO of <span style={{ color: '#F44336' }}>@use-hooks/<%- name %></span></h2>
     </div>
   );
 }
