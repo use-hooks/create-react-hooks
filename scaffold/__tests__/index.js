0d80e35080e03b8a90a338fcd464fe0a1d777dfd
Cody Chan <int64ago@gmail.com>
2019-04-15 19:52:06 +0800

    feat: update templates

--- scaffold/__tests__/index.js
+++ scaffold/__tests__/index.js
@@ -1,5 +1,9 @@
-describe('hooks test', () => {
-  it('should be a function', () => {
-    expect(require('../src/index').default).toEqual(expect.any(Function));
-  });
+// import '@babel/polyfill';
+// import React from 'react';
+// import { render } from 'react-testing-library';
+
+import hooks from '../src/index';
+
+it('should be a function', () => {
+  expect(hooks).toEqual(expect.any(Function));
 });
